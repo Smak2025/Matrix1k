f604cc8a3f3796a42e095e6316965867ffd3c83e
Smak2025 <smak-80+2025@yandex.ru>
2025-11-17 09:58:16 +0300

    Matrix лекции

--- src/ru/gr050x/Main.java
+++ src/ru/gr050x/Main.java
@@ -1,4 +1,24 @@
 package ru.gr050x;
 
 public class Main {
+    public static void main(String[] args) {
+        var m1 = new Matrix(5, 3);
+        System.out.println(m1);
+        var arr2 = new int[][]{{1, 2}, {3, 4}};
+        var m2 = new Matrix(arr2);
+        System.out.println(m2);
+        arr2[0][1] = 10000;
+        System.out.println(m2);
+        var arr1 = new int[]{5, 6, 7, 8};
+        var m3 = new Matrix(2, 2, arr1);
+        System.out.println(m3);
+        var m4 = new Matrix(1, 4, arr1);
+        System.out.println(m4);
+        var m5 = new Matrix(4, 1, arr1);
+        System.out.println(m5);
+
+        var arr12 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        var m6 = new Matrix(3, 3, arr12);
+        System.out.println(m6.getElem(5));
+    }
 }
--- src/ru/gr050x/Matrix.java
+++ src/ru/gr050x/Matrix.java
@@ -1,4 +1,61 @@
 package ru.gr050x;
 
+import java.util.Arrays;
+
 public class Matrix {
+    private final int[][] elem;
+
+    public Matrix(int rows, int cols){
+        elem = new int[rows][cols];
+    }
+
+    public Matrix(int[][] elem){
+        this(elem.length, elem[0].length);
+        for (int i = 0; i < elem.length; i++){
+            this.elem[i] = Arrays.copyOf(elem[i], elem[i].length);
+//            for (int j = 0; j < elem[i].length; j++){
+//                this.elem[i][j] = elem[i][j];
+//            }
+        }
+    }
+
+    public Matrix(int rows, int cols, int[] elems){
+        //TODO: Исправить!
+        this(rows, cols);
+        for (int i = 0; i < rows; i++){
+            elem[i] = Arrays.copyOfRange(elems, cols*i, cols*(i+1));
+        }
+    }
+
+
+    public String toString(){
+        var sMatrix = new StringBuilder();
+        for (int i = 0; i < elem.length; i++){
+            sMatrix.append('[');
+            for(int j = 0; j < elem[i].length; j++){
+                sMatrix.append(elem[i][j]);
+                if (j < elem[i].length - 1) sMatrix.append(' ');
+            }
+            sMatrix.append("]\n");
+        }
+        return sMatrix.toString();
+    }
+
+    public int rowCount(){
+        return elem.length;
+    }
+
+    public int colCount(){
+        return elem[0].length;
+    }
+
+    public int getElem(int i, int j) {
+        return elem[i][j];
+    }
+
+    public int getElem(int index){
+        return elem[index / colCount()][index % colCount()];
+    }
+
+
 }
